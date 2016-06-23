3ccda272fc99d5afb0d185d94e1a279953e90844
oded leiba <oded@colu.co>
2016-06-23 12:14:19 +0300

    fix tests and linters

--- builder.js
+++ builder.js
@@ -1,94 +1,93 @@
 var _ = require('lodash')
 
 module.exports = function (squel) {
-	var squel = squel || require('squel')
-	var builder = new squel.cls.QueryBuilder()
-	return {
-		to_values: function (values) {
-			return builder._formatValueForQueryString(values)
-		},
-		to_value: function (value) {
-			return builder._formatValueForQueryString(value)
-		},
+  squel = squel || require('squel')
+  var builder = new squel.cls.QueryBuilder()
+  return {
+    to_values: function (values) {
+      return builder._formatValueForQueryString(values)
+    },
+    to_value: function (value) {
+      return builder._formatValueForQueryString(value)
+    },
 		// Input: seuqelize model
-		to_columns_of_model: function (model, options) {
-			var self = this
-		  var columns = []
-		  var table_name = model.getTableName()
-		  Object.keys(model.attributes).forEach(function (attribute) {
-		    if (!options || !options.exclude || options.exclude.indexOf(attribute) === -1) {
-		      columns.push(self.to_column(table_name + '.' + attribute))
-		    }
-		  })
-		  return columns.join(', ')
-		},
-		to_columns: function (attributes) {
-			if (!attributes || !Array.isArray(attributes)) {
-				throw new Error('attributes must be an array')
-			}
-		  return attributes.map(this.to_column).join(', ')
-		},
-		to_column: function (attribute) {
-		  return builder._formatFieldName(attribute)
-		},
-		to_update_string: function (update_object) {
-			if (!update_object || !_.isObject(update_object)) {
-				throw new Error('update_object must be an object')
-			}
-		  var columns = this.to_columns(Object.keys(update_object))
-		  var values = this.to_values(_.values(update_object))
-		  return '(' + columns + ') = ' + values
-		},
-		to_multi_condition_update: function (bulk) {
-			var self = this
-		  var table_name = bulk.table_name
-		  var updates = bulk.updates
-		  var attributes_to_set = {}
-		  var conditions = updates.map(function (update) { return update.where })
+    to_columns_of_model: function (model, options) {
+      var self = this
+      var columns = []
+      var table_name = model.getTableName()
+      Object.keys(model.attributes).forEach(function (attribute) {
+        if (!options || !options.exclude || options.exclude.indexOf(attribute) === -1) {
+          columns.push(self.to_column(table_name + '.' + attribute))
+        }
+      })
+      return columns.join(', ')
+    },
+    to_columns: function (attributes) {
+      if (!attributes || !Array.isArray(attributes)) {
+        throw new Error('attributes must be an array')
+      }
+      return attributes.map(this.to_column).join(', ')
+    },
+    to_column: function (attribute) {
+      return builder._formatFieldName(attribute)
+    },
+    to_update_string: function (update_object) {
+      if (!update_object || !_.isObject(update_object)) {
+        throw new Error('update_object must be an object')
+      }
+      var columns = this.to_columns(Object.keys(update_object))
+      var values = this.to_values(_.values(update_object))
+      return '(' + columns + ') = ' + values
+    },
+    to_multi_condition_update: function (bulk) {
+      var self = this
+      var table_name = bulk.table_name
+      var updates = bulk.updates
+      var attributes_to_set = {}
+      var conditions = updates.map(function (update) { return update.where })
 
-		  updates.forEach(function (update) {
-		    Object.keys(update.set).forEach(function (attribute) {
-		      attributes_to_set[attribute] = attributes_to_set[attribute] || []
-		      attributes_to_set[attribute].push({
-		        value: update.set[attribute],
-		        conditions: update.where
-		      })
-		    })
-		  })
-		  var sql_query = 'UPDATE ' + table_name + '\n'
-		  sql_query += '  SET '
-		  sql_query += Object.keys(attributes_to_set).map(function (attribute) {
-		    var attribute_to_set_clause = self.to_column(attribute) + ' = CASE\n'
-		    attributes_to_set[attribute].forEach(function (obj) {
-		      attribute_to_set_clause += '    WHEN ' + self.to_condition(obj.conditions) + ' THEN ' + self.to_value(obj.value) + '\n'
-		    })
-		    attribute_to_set_clause += '  END'
-		    return attribute_to_set_clause
-		  }).join(',\n') + '\n'
-		  sql_query += 'WHERE ' + self.to_conditions(conditions)
-		  return sql_query
-		},
-		to_conditions: function (conditions) {
-			var self = this
-			if (!conditions || !Array.isArray(conditions)) {
-				throw new Error('conditions must be an array')
-			}
-		  var expr = squel.expr()
-		  conditions.forEach(function (condition) {
-		  	expr.or(self.to_condition(condition))
-		  })
-		  return '(' + expr.toString() + ')'
-		},
-		to_condition: function (condition) {
-			var self = this
-			if (!condition || !_.isObject(condition)) {
-				throw new Error('condition must be an object')
-			}
-		  var expr = squel.expr('hello')
-		  Object.keys(condition).forEach(function (key) {
-		  	expr.and(builder._formatFieldName(key) + ' = ?', condition[key])
-		  })
-		  return '(' + expr.toString() + ')'
-		}
-	}
+      updates.forEach(function (update) {
+        Object.keys(update.set).forEach(function (attribute) {
+          attributes_to_set[attribute] = attributes_to_set[attribute] || []
+          attributes_to_set[attribute].push({
+            value: update.set[attribute],
+            conditions: update.where
+          })
+        })
+      })
+      var sql_query = 'UPDATE ' + table_name + '\n'
+      sql_query += '  SET '
+      sql_query += Object.keys(attributes_to_set).map(function (attribute) {
+        var attribute_to_set_clause = self.to_column(attribute) + ' = CASE\n'
+        attributes_to_set[attribute].forEach(function (obj) {
+          attribute_to_set_clause += '    WHEN ' + self.to_condition(obj.conditions) + ' THEN ' + self.to_value(obj.value) + '\n'
+        })
+        attribute_to_set_clause += '  END'
+        return attribute_to_set_clause
+      }).join(',\n') + '\n'
+      sql_query += 'WHERE ' + self.to_conditions(conditions)
+      return sql_query
+    },
+    to_conditions: function (conditions) {
+      var self = this
+      if (!conditions || !Array.isArray(conditions)) {
+        throw new Error('conditions must be an array')
+      }
+      var expr = squel.expr()
+      conditions.forEach(function (condition) {
+        expr.or(self.to_condition(condition))
+      })
+      return '(' + expr.toString() + ')'
+    },
+    to_condition: function (condition) {
+      if (!condition || !_.isObject(condition)) {
+        throw new Error('condition must be an object')
+      }
+      var expr = squel.expr('hello')
+      Object.keys(condition).forEach(function (key) {
+        expr.and(builder._formatFieldName(key) + ' = ?', condition[key])
+      })
+      return '(' + expr.toString() + ')'
+    }
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,47 +1,47 @@
 /* eslint-env mocha */
 var path = require('path')
 var assert = require('assert')
+var squel = require('squel')
 
 describe('Check SQL builder', function () {
-	var options = {
-		autoQuoteFieldNames: true,
-		nameQuoteCharacter: '"'
-	}
-	var builder = require(path.join(__dirname, '../builder.js'))(options)
-	it('to_value number', function (done) {
-		assert.equal(builder.to_value(5), '5')
-		done()
-	})
-	it('to_value string', function (done) {
-		assert.equal(builder.to_value('foo'), '\'foo\'')
-		done()
-	})
-	it('to_column', function (done) {
-		assert.equal(builder.to_column('someColumn'), '"someColumn"')
-		done()
-	})
-	it('to_columns', function (done) {
-		assert.equal(builder.to_columns(['someColumn', 'otherColumn']), '"someColumn", "otherColumn"')
-		done()
-	})
-	it('to_columns_of_model', function (done) {
-		var model = {
-			getTableName: function () { return 'someTable' },
-			attributes: {someColumn: 'SMALLINT', otherColumn: 'BOOLEAN', toExcludeColumn: 'TEXT'}
-		}
-		assert.equal(builder.to_columns_of_model(model, {exclude: ['toExcludeColumn']}), '"someTable"."someColumn", "someTable"."otherColumn"')
-		done()
-	})
-	it('to_update_string', function (done) {
-		assert.equal(builder.to_update_string({color: 'green', age: 25}), '("color", "age") = (\'green\', 25)')
-		done()
-	})
-	it('to_condition', function (done) {
-		assert.equal(builder.to_condition({color: 'green', age: 25}), '("color" = \'green\' AND "age" = 25)')
-		done()
-	})
-	it('to_conditions', function (done) {
-		assert.equal(builder.to_conditions([{color: 'green', age: 25}, {name: 'david', id: 932}]), '(("color" = \'green\' AND "age" = 25) OR ("name" = \'david\' AND "id" = 932))')
-		done()
-	})
+  squel.cls.DefaultQueryBuilderOptions.autoQuoteFieldNames = true
+  squel.cls.DefaultQueryBuilderOptions.nameQuoteCharacter = '"'
+  squel.cls.DefaultQueryBuilderOptions.separator = '\n'
+  var builder = require(path.join(__dirname, '../builder.js'))(squel)
+  it('to_value number', function (done) {
+    assert.equal(builder.to_value(5), '5')
+    done()
+  })
+  it('to_value string', function (done) {
+    assert.equal(builder.to_value('foo'), '\'foo\'')
+    done()
+  })
+  it('to_column', function (done) {
+    assert.equal(builder.to_column('someColumn'), '"someColumn"')
+    done()
+  })
+  it('to_columns', function (done) {
+    assert.equal(builder.to_columns(['someColumn', 'otherColumn']), '"someColumn", "otherColumn"')
+    done()
+  })
+  it('to_columns_of_model', function (done) {
+    var model = {
+      getTableName: function () { return 'someTable' },
+      attributes: {someColumn: 'SMALLINT', otherColumn: 'BOOLEAN', toExcludeColumn: 'TEXT'}
+    }
+    assert.equal(builder.to_columns_of_model(model, {exclude: ['toExcludeColumn']}), '"someTable"."someColumn", "someTable"."otherColumn"')
+    done()
+  })
+  it('to_update_string', function (done) {
+    assert.equal(builder.to_update_string({color: 'green', age: 25}), '("color", "age") = (\'green\', 25)')
+    done()
+  })
+  it('to_condition', function (done) {
+    assert.equal(builder.to_condition({color: 'green', age: 25}), '("color" = \'green\' AND "age" = 25)')
+    done()
+  })
+  it('to_conditions', function (done) {
+    assert.equal(builder.to_conditions([{color: 'green', age: 25}, {name: 'david', id: 932}]), '(("color" = \'green\' AND "age" = 25) OR ("name" = \'david\' AND "id" = 932))')
+    done()
+  })
 })
